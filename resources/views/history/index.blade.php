e99e92be86ee1ce7ec522e095d998a6590aa34ae
b5810451152 <anaphat.i@ku.th>
2018-04-25 16:00:13 +0700

    Sortable

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -1,8 +1,50 @@
 @extends('layout.go')
 
 
+@push('script')
+<script src="//code.jquery.com/jquery-1.12.3.js"></script>
+<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script
+src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet"
+href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet"
+href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
+
+
+
+<script>
+$(document).ready(function() {
+$('#table').DataTable();
+} );
+
+
+
+$(document).on('click', '.edit-modal', function() {
+    $('#footer_action_button').text(" Update");
+    $('#footer_action_button').addClass('glyphicon-check');
+    $('#footer_action_button').removeClass('glyphicon-trash');
+    $('.actionBtn').addClass('btn-success');
+    $('.actionBtn').removeClass('btn-danger');
+    $('.actionBtn').removeClass('delete');
+    $('.actionBtn').addClass('edit');
+    $('.modal-title').text('Edit');
+    $('.deleteContent').hide();
+    $('.form-horizontal').show();
+    var stuff = $(this).data('info').split(',');
+    fillmodalData(stuff)
+    $('#myModal').modal('show');
+});
+</script>
+
+
+@endpush
+
+
 
 @section('content')
+
+<br>
 <table class="table" id="table">
     <thead>
         <tr>
@@ -18,19 +60,25 @@
     </thead>
     <tbody>
     @foreach($leaves as $item)
+
     <tr class="item{{$item->id}}">
+
       <td>{{ $item->id }}</td>
-      <td>{{ $item->subordinate_id }}</td>
+      <td>{{ $item->full_name }}</td>
       <td>{{ $item->description }}</td>
       <td>{{ $item->substitute_id }}</td>
       <td>{{ $item->leave_type }}</td>
-      <td>{{ $item->is_enabled }}</td>
-      <td>{{ $item->is_approved }}</td>
+      <td>{!! $item->is_enabled ? '<i class="fa fa-check"></i>' : '<i class="fa fa-times"></i>' !!}</td>
+      <td>{!! $item->is_approved ? '<i class="fa fa-check"></i>' : '<i class="fa fa-times"></i>' !!}</td>
+
 
     </tr>
     @endforeach
     </tbody>
 </table>
+
+
+{{ Auth::user()->id }}
 
 
 
