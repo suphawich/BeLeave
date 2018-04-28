24c12fee3ddb52382025d67b0cd3c4518b9e5862
suphawich <31471680+suphawich@users.noreply.github.com>
2018-04-28 15:46:58 +0700

    rollback (Mark) Noti

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+use App\User;
+
+class NotificationsController extends Controller
+{
+    public function markAsRead_manageRequestLeave(Request $request) {
+        Auth::user()->unreadnotifications->markAsRead();
+        return redirect('/manage/request/leave');
+        // return $user;
+    }
+}
