8499b9d301a40ead079a8ef070eb4de670f04860
b5810451152 <anaphat.i@ku.th>
2018-04-28 13:07:04 +0700

    .

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -67,84 +67,90 @@
 
 
 
-                        <div class="">
-                          <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-                        <ul class="navbar-nav">
-                          <li class="nav-item active">
-                            <a class="nav-link" href="#">FEATURE </a>
-                          </li>
-
-                        </ul>
-                      </nav>
+        </div>
 
 
-                        <div class="row">
+        <div class="row ml-1" style="width:99.5%">
+          <div class="card col-12 mt-2" >
+            <div class="">
+              <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">FEATURE </a>
+              </li>
 
-                          <div class="col-6 ">
-
-
-
-
-
-
-                          <div class="w3-container">
-
-
-                            <div class="w3-card-4" style="width:100%">
-                              <header class="w3-container w3-light-grey">
-                                <h3>John Doe</h3>
-                              </header>
-                              <div class="w3-container">
-                                <p>1 new friend request</p>
-                                <hr>
-                                <img src="images/BGtest.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                                <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p><br>
-                              </div>
-                              <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
-                            </div>
-                          </div>
-                          </div>
+            </ul>
+          </nav>
 
 
-                          <br>
+            <div class="row ">
 
-                          <div class="col-6">
+              <div class="col-6 ">
 
 
 
 
 
 
-
-                          <div class="w3-container">
-
+              <div class="w3-container">
 
 
-                            <div class="w3-card-4" style="width:100%">
-                              <header class="w3-container w3-light-grey">
-                                <h3>John Doe</h3>
-                              </header>
-                              <div class="w3-container">
-                                <p>1 new friend request</p>
-                                <hr>
-                                <img src="images/slide2.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                                <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p><br>
-                              </div>
-                              <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
-                            </div>
-                          </div>
-                          </div>
-                        </div>
-                          <br>
+                <div class="w3-card-4" style="width:100%">
+                  <header class="w3-container w3-light-grey">
+                    <h3>John Doe</h3>
+                  </header>
+                  <div class="w3-container">
+                    <p>1 new friend request</p>
+                    <hr>
+                    <img src="images/BGtest.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                    <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p><br>
+                  </div>
+                  <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
+                </div>
+              </div>
+              </div>
 
-                        </div>
 
+              <br>
+
+              <div class="col-6  ">
+
+
+
+
+
+
+
+              <div class="w3-container">
+
+
+
+                <div class="w3-card-4" style="width:100%">
+                  <header class="w3-container w3-light-grey">
+                    <h3>John Doe</h3>
+                  </header>
+                  <div class="w3-container">
+                    <p>1 new friend request</p>
+                    <hr>
+                    <img src="images/slide2.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                    <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p><br>
+                  </div>
+                  <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
+                </div>
+              </div>
+              </div>
+            </div>
+              <br>
+
+            </div>
+
+          </div>
 
 
 
         </div>
 
-        <div class="col-4  pl-1 pr-1 ml-3  card">
+        <div class="col-4 mt-2 pl-1 pr-1 ml-3 ">
 
           <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
@@ -165,18 +171,20 @@
           <div class="w3-container">
 
 
-            <div class="w3-card-4" style="width:100%">
-              <header class="w3-container w3-light-grey">
-                <h3>John Doe</h3>
-              </header>
-              <div class="w3-container">
-                <p>1 new friend request</p>
-                <hr>
-                <img src="images/slide2.jpg" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                <p>CEO at Mighty Schools. Marketing and Advertising. Seeking a new job and new opportunities.</p><br>
-              </div>
-              <button class="w3-button w3-block w3-dark-grey">+ Connect</button>
-            </div>
+            <div class="row">
+      <div class="col s12 m6">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/slide1.jpg" style="width:100%">
+            <span class="card-title">Card Title</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+      </div>
+    </div>
           </div>
 
           <br>
