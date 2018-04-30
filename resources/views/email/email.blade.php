eb318d8f5d2936c7af46b680cc2d1f89c11137fa
kittichai <bankky@kittichais-MacBook-Pro.local>
2018-04-30 19:57:13 +0700

    --

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -6,7 +6,8 @@
 <body>
 <h1>Welcome to Website BeLeave</h1>
 <h2>Hi {{$user->full_name}} Thanks! for register </h2>
-<h2>Your registered email-id is {{$user->email}} and Your password is {{$pass}}</h2>
+<h2>Your registered email-id is {{$user->email}} <br> Your password is {{$pass}}</h2>
+<h2>Please change your password for your privacy and safety</h2>
 <br/>
 </body>
 </html>
