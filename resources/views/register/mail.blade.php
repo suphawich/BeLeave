2f80af1f68d74188d509bac39b5167471a25e5a4
kittichai <bankky@kittichais-MacBook-Pro.local>
2018-04-30 20:20:13 +0700

    edit send mail after subordinate regis

---
+++ resources/views/register/mail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<h1>Welcome to Website BeLeave</h1>
+<h2>Hi {{$user->full_name}} Thanks! for register </h2>
+<h2>Your registered email-id is {{$user->email}} <br> Your password is {{$pass}}</h2>
+<h2>Please change your password for your privacy and safety</h2>
+<br/>
+</body>
+</html>
